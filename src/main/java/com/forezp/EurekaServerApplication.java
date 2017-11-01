3d3fbcf47e7f48ee869fd58a19fb6b8750073968
zpsfly <451714977@163.com>
2017-11-01 15:24:05 +0800

    this is a commit

---
+++ src/main/java/com/forezp/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+/**
+ * 
+ * @EnableEurekaServer 服务注册服务端
+ * @SpringBootApplication spring boot启动器
+ * @author Administrator
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
