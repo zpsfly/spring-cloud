5110c2389e98f38d89cbed44fe2e437f5e27fc94
zpsfly <451714977@qq.com>
2018-05-17 22:48:51 +0800

    oks

---
+++ src/main/java/com/spring/cloud/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+/**
+ * 
+ * @EnableEurekaServer 服务注册服务端
+ * @SpringBootApplication spring boot启动器
+ * @author Administrator
+ *  this git pro
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
